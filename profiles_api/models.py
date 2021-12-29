c521a38042d91241707be0931e3bd5cfc5d8dc3a
Jisun Kim <jisun@Jisuns-MacBook-Air.local>
2021-12-29 22:34:53 +0900

    corrected typo, done creatsuperuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+
 # Create your models here.
 
+# email: mark@londonappdev.com
+# name: mark
+# pw: p123
 
-class UserProfileManager():
+
+class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
@@ -13,11 +18,13 @@ class UserProfileManager():
         if not email:
             raise ValueError('user must have an email address')
 
-        email = self.normarlize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
+
+        return user
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
@@ -39,7 +46,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     objects = UserProfileManager()
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDs = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
